fce4d55eb84bfd79dfa22fdfbb5be92bed0d6065
Karikh Dmitry <dmitry.karikh@saydanke.ru>
2018-02-13 14:50:42 +0300

    add default parameters

--- lib/main/client_storage.php
+++ lib/main/client_storage.php
@@ -13,7 +13,7 @@ class ClientStorage
         $APPLICATION->set_cookie($strName, $mixValue);
     }
 
-    public static function get($strName)
+    public static function get($strName, $mixDefault)
     {
         global $APPLICATION;
 
@@ -21,6 +21,10 @@ class ClientStorage
 
         if (isset($_SESSION[$strName])) {
             $mixValue = $_SESSION[$strName];
+        }
+
+        if ($mixValue === '' || is_null($mixValue)) {
+            $mixValue = $mixDefault;
         }
 
         return $mixValue;
