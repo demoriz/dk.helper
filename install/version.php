65a18d595839598e50e0dbcef7dc1de92ec5acd4
demoriz <demoriz@gmail.com>
2018-11-29 11:35:29 +0300

    добавил Price::getDiscountPrice($intProductElementID, $intCatalogGroupID)

--- install/version.php
+++ install/version.php
@@ -1,5 +1,5 @@
 <?php
 $arModuleVersion = array(
-    "VERSION" => "0.1.3",
-    "VERSION_DATE" => "2018-11-14 13:10:00",
+    "VERSION" => "0.1.4",
+    "VERSION_DATE" => "2018-11-29 11:34:00",
 );
